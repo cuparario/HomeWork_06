906ec2a4fe714463c708243d621c392808b62160
Рамиль Шарапов <cuparario@yandex.ru>
2023-12-07 11:09:20 +0300

    Добавили домашнее задание 6

---
+++ Zadacha_02/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задайте строку, содержащую латинские буквы в обоих регистрах. 
+Сформируйте строку, в которой все заглавные буквы заменены на 
+строчные. */
+
+using System;
+public class Zadacha_02
+{
+    public static void Main(string[] args)
+    {
+        string input = "AbCdEfGhIjK";
+        string result = input.ToLower();
+        Console.WriteLine(result);
+    }
+}
---
+++ Zadacha_03/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задайте произвольную строку. Выясните, является ли она палиндромом. */
+
+using System;
+public class Zadacha_03
+{
+public static void Main(string[] args)
+    {
+        string input = "Хоп Хей";
+        string result = ReverseWords(input);
+        Console.WriteLine(result);
+    }
+public static string ReverseWords(string str)
+    {
+        string[] words = str.Split(' ');
+        Array.Reverse(words);
+        return string.Join(" ", words);
+    }
+}
